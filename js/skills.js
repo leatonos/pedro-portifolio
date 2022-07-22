c3102fa21896f9a35cccbd79c9634cdb3f61bf1e
leatonos <pedrofmattosb@gmail.com>
2022-07-22 15:55:24 -0400

    Added new skills

--- js/skills.js
+++ js/skills.js
@@ -2,10 +2,16 @@ $(document).ready(function(){
 
     //List of my skills
     let skills = [
+        {title:"HTML5",img:"img/icons/html.svg",desc:"Something about PHP"},
         {title:"Javascript",img:"img/icons/javascript-icon.svg",desc:"Something about Javascript"},
         {title:"CSS",img:"img/icons/css3-icon.svg",desc:"Something about CSS"},
         {title:"React.js",img:"img/icons/react.svg",desc:"Something about React.js"},
-        {title:"php",img:"img/icons/php.svg",desc:"Something about PHP"}
+        {title:"php",img:"img/icons/php.svg",desc:"Something about PHP"},
+        {title:"Firebase",img:"img/icons/firebase.svg",desc:"Something about PHP"},
+        {title:"Mysql",img:"img/icons/mysql.svg",desc:"Something about PHP"},
+        {title:"Figma",img:"img/icons/figma.svg",desc:"Something about PHP"},
+        {title:"Wordpress",img:"img/icons/wordpress.svg",desc:"Something about PHP"},
+
     ]
     
 
